c95ffd6788952200b0f15ced5845f23811ccb1b3
nick3fc <nick3fc@gmail.com>
2026-06-20 15:38:25 +0300

    hw12-1.1

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,8 @@
 // -----------lib connection code----------
 import SimpleLightbox from 'simplelightbox';
-// import * as SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const lightbox = new SimpleLightbox.default('.gallery a', {
+const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
   showCounter: false,
